f0f338fe877203bcdc699aa590920c74b1d8b33e
RafalDembowski <dembowskirafal96@gmail.com>
2020-12-01 00:43:03 +0100

    add class RoomMateDbContext and connection string to web.config

---
+++ RoomMate.Data/Context/RoomMateDbContext.cs
@@ -0,0 +1,28 @@
+﻿using System.Data.Entity;
+using RoomMate.Entities.Users;
+using RoomMate.Entities.Rooms;
+using RoomMate.Entities.Bookings;
+
+namespace RoomMate.Data.Context
+{
+    public class RoomMateDbContext : DbContext
+    {
+        public RoomMateDbContext() : base("Name=RoomMateDbConString")
+        {
+
+        }
+        public DbSet<User> Users { get; set; }
+        public DbSet<UserImage> UserImages {get; set;}
+        public DbSet<Room> Room { get; set; }
+        public DbSet<RoomImage> RoomImages { get; set; }
+        public DbSet<Equipment> Equipments { get; set; }
+        public DbSet<Address> Addresses { get; set; }
+        public DbSet<Booking> Bookings { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+
+        }
+
+    }
+}
