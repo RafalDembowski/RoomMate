77080f68ff3f3e8bb6e314cc6e70253374d24fb8
RafalDembowski <dembowskirafal96@gmail.com>
2020-12-03 13:59:50 +0100

    rename UnitTest1.cs from RoomMate.Data.Test to GenericRepositoryTests.cs and instal Moq package

---
+++ RoomMate.Data.Test/GenericRepositoryTests.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace RoomMate.Data.Test
+{
+    [TestClass]
+    public class GenericRepositoryTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
