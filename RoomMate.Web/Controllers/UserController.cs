3aff8b5d3227fc2bde058b6786e96ebf6ae7f471
RafalDembowski <dembowskirafal96@gmail.com>
2020-12-04 23:34:15 +0100

    create folders Css and Images. Create style.css. Create UserController.cs

---
+++ RoomMate.Web/Controllers/UserController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using RoomMate.Data.Repository;
+using RoomMate.Entities.Users;
+
+namespace RoomMate.Controllers
+{
+    public class UserController : Controller
+    {
+        private IGenericRepository<User> userRepository = null;
+
+        public UserController()
+        {
+            this.userRepository = new GenericRepository<User>();
+        }
+        public UserController(IGenericRepository<User> userRepository)
+        {
+            this.userRepository = userRepository;
+        }
+
+        [HttpGet]
+        public ActionResult Registration()
+        {
+            return View();
+        }
+    }
+}
