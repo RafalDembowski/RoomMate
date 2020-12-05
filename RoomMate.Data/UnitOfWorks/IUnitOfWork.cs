acd19c5c7fe3b463c92413df9731f4696d8c0f11
RafalDembowski <dembowskirafal96@gmail.com>
2020-12-05 19:43:33 +0100

    add an interface IUnitOfWork and implement this in UnitOfWork.cs

---
+++ RoomMate.Data/UnitOfWorks/IUnitOfWork.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RoomMate.Data.Repository;
+
+namespace RoomMate.Data.UnitOfWorks
+{
+    interface IUnitOfWork : IDisposable
+    {
+        IUserRepository UsersRepository { get; }
+        int Complete();
+    }
+}
---
+++ RoomMate.Data/UnitOfWorks/UnitOfWork.cs
@@ -0,0 +1,29 @@
+﻿using RoomMate.Data.Context;
+using RoomMate.Data.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace RoomMate.Data.UnitOfWorks
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly RoomMateDbContext _context;
+        public IUserRepository UsersRepository { get; }
+
+        public UnitOfWork(RoomMateDbContext context)
+        {
+            _context = context;
+            UsersRepository = new UserRepository(_context);
+        }
+        public int Complete()
+        {
+            return _context.SaveChanges();
+        }
+        public void Dispose()
+        {
+            _context.Dispose();
+        }
+    }
+}
