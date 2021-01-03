2cedf7da7fc8b5ec7e0a2e160031e5725c1a94f5
RafalDembowski <dembowskirafal96@gmail.com>
2021-01-03 16:06:44 +0100

    add room controller and displayRoom.cshtml

---
+++ RoomMate.Web/Controllers/RoomController.cs
@@ -0,0 +1,39 @@
+﻿using RoomMate.Data.Context;
+using RoomMate.Data.UnitOfWorks;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace RoomMate.Controllers
+{
+    public class RoomController : Controller
+    {
+        private UnitOfWork unitOfWork;
+        public RoomController()
+        {
+            unitOfWork = new UnitOfWork(new RoomMateDbContext());
+
+        }
+        [HttpGet]
+        public ActionResult DisplayRoom(string id)
+        {
+            bool codeActivationCanBeGuid = Guid.TryParse(id, out var newGuid);
+
+            if (!String.IsNullOrEmpty(id) && codeActivationCanBeGuid == true && !id.Equals("00000000-0000-0000-0000-000000000000"))
+            {
+                var room = unitOfWork.RoomsRepository.Get(filter: r => r.RoomID == new Guid(id),
+                                                          orderBy: null,
+                                                          includeProperties: "Address,Equipment")
+                                                          .FirstOrDefault();
+                return View(room);
+            }
+
+            return RedirectToAction("Index", "Home");
+
+        }
+
+
+    }
+}
